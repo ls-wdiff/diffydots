c8e77d26abb25aea88bbc11621610b635adb85ec
ls-wdiff <nanotech64@gmail.com>
2025-01-11 16:47:26 -0800

    Add files via upload

---
+++ config/waybar/scripts/theme.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin/"
+
+CURRENTIMG="$(<~/.cache/wal/wal )"
+MODE=""
+
+if [[ -e "${HOME}/.cache/wal/mode" ]]; then
+    MODE="$(<~/.cache/wal/mode )"
+else
+    cd ${HOME}/.cache/wal && touch mode
+
+    set -o noclobber
+    echo "dark" >| ${HOME}/.cache/wal/mode
+
+    MODE="dark"
+fi
+
+if [[ $MODE = "light" ]]; then
+    notify-send "Changing to dark theme..."
+
+    MODE="dark"
+
+    wal -i $CURRENTIMG
+
+    set -o noclobber
+    echo "dark" >| ${HOME}/.cache/wal/mode
+
+    notify-send "Changed to dark theme!"
+else
+    notify-send "Changing to light theme..."
+
+    MODE="light"
+
+    wal -i $CURRENTIMG
+
+    set -o noclobber
+    echo "light" >| ${HOME}/.cache/wal/mode
+
+    notify-send "Changed to light theme!"
+fi
